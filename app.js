e82607fd6e7c6b2d112f3e1c4872cd8e3134079b
kudoshinichi2k5 <23520797@gm.uit.edu.vn>
2025-12-08 17:13:05 +0700

    chore(api): add draft API endpoints as comments

--- app.js
+++ app.js
@@ -4,10 +4,26 @@ const body_parser = require("body-parser");
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./docs/swagger');
 
+// const userRouter = require('./routers/user_router');
+// const mentorRouter = require('./routers/mentor_router');
+// const opportunityRouter = require('./routers/opportunity_router');
+// const articleRouter = require('./routers/article_router');
+// const bookmarkRouter = require('./routers/bookmark_router');
+// const chatbotRouter = require('./routers/chatbot_router');
+
 const app = express();
 app.use(body_parser.json());
 
 // Url api để mở UI Swagger (thấy được mô tả các API)
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));  
+
+// Routers
+// app.use('/api/user', userRouter);
+// app.use('/api/mentor', mentorRouter);
+// app.use('/api/opportunity', opportunityRouter);
+// app.use('/api/article', articleRouter);
+// app.use('/api/bookmark', bookmarkRouter);
+// app.use('/api/chatbot', chatbotRouter);
+
 
 module.exports = app;
